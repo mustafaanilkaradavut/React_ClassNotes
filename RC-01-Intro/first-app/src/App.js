d362e128bf1c5c099124848935c4d05275be1e78
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-17 11:01:45 +0100

    end_of_react_intro

--- RC-01-Intro/first-app/src/App.js
+++ RC-01-Intro/first-app/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+//? Component
+//__  JSX bu js içerisindedir. Baş Harfi büyük harfle başlar. Return'ü JSX'tir.
+//__  JSX ise HTML ve JS'nin birleşmiş halidir.
+//__ index.html içerisinde render edilir.
 
 function App() {
+  //? JS KODU : function ve return arasında yazılabilir. JS komutlarıda aşağıda return içinde kullanılabilir.
+  const deneme = "REACT";
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +21,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
