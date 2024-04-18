1ef17e572725b6dc8ec1cceaf491d2b0f49194c3
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-18 23:16:18 +0100

    up

--- RC-03-Styling/module-css/src/App.js
+++ RC-03-Styling/module-css/src/App.js
@@ -1,30 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-//? Component
-//__  JSX bu js içerisindedir. Baş Harfi büyük harfle başlar. Return'ü JSX'tir.
-//__  JSX ise HTML ve JS'nin birleşmiş halidir.
-//__ index.html içerisinde render edilir.
-
+import data from "./data";
 function App() {
-  //? JS KODU : function ve return arasında yazılabilir. JS komutlarıda aşağıda return içinde kullanılabilir.
-  const deneme = "REACT";
-
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h2 style={{ textAlign: "center" }}>LANGUAGES</h2>
     </div>
   );
 }
