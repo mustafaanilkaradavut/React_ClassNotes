5d5494b9600a385fe8c199c3acff3cec35c16a3d
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-19 03:10:19 +0100

    sass

---
+++ RC-03-Styling/sass.styling/src/App.js
@@ -0,0 +1,11 @@
+import data from "./data";
+function App() {
+  console.log(data);
+  return (
+    <div className="App">
+      <h2 style={{ textAlign: "center" }}>LANGUAGES</h2>
+    </div>
+  );
+}
+
+export default App;
