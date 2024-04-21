3c439030b4d29e137e70f9c993c3110e5da9525c
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-21 07:40:38 +0100

    appjs

--- RC-04-Product-list/src/App.js
+++ RC-04-Product-list/src/App.js
@@ -1,11 +1,15 @@
 import Header from "./component/header/Header";
 import ProductList from "./component/products/ProductList";
 
+import { products, categories } from "./helper/data"; //__ Named export olduğu için süslü içinde yazarız.
+console.log(products, categories);
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ProductList />
+      <Header categories={categories} text="Product List" />{" "}
+      {/*   key = {value}  */}
+      <ProductList products={products} />
     </div>
   );
 }
