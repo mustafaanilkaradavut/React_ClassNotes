271ebc2f4f5fe999c39185827b2e18b90d557447
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-25 19:34:56 +0100

    forObject

--- RC-06-Forms-otherEvents/src/App.js
+++ RC-06-Forms-otherEvents/src/App.js
@@ -1,4 +1,5 @@
 import Forms from "./component/1-forms/Forms";
+import FormsObject from "./component/1-forms/FormsObject";
 import KeyboardClipboard from "./component/2-keyboardClipboardEvents/KeyboardClipboardEvents";
 import MouseEvent from "./component/3-mouseEvents/MouseEvent";
 import Container from "react-bootstrap/Container";
@@ -6,7 +7,7 @@ import Container from "react-bootstrap/Container";
 function App() {
   return (
     <Container className="mt-4">
-      <Forms />
+      <FormsObject />
       {/* <KeyboardClipboard />
       <MouseEvent /> */}
     </Container>
