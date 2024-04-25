e1f95afc58df445163034ea1eaadfcaafbebe0fa
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-04-25 17:30:43 +0100

    react bootstrap

--- RC-06-Forms-otherEvents/src/App.js
+++ RC-06-Forms-otherEvents/src/App.js
@@ -1,9 +1,15 @@
+import Forms from "./component/1-forms/Forms";
+import KeyboardClipboard from "./component/2-keyboardClipboardEvents/KeyboardClipboardEvents";
+import MouseEvent from "./component/3-mouseEvents/MouseEvent";
+import Container from "react-bootstrap/Container";
+
 function App() {
-  console.log();
   return (
-    <div className="App">
-      <h2 style={{ textAlign: "center" }}>LANGUAGES</h2>
-    </div>
+    <container className="mt-4">
+      <Forms />
+      <KeyboardClipboard />
+      <MouseEvent />
+    </container>
   );
 }
 
