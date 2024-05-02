b938ae8d7ab445d775345bb5d14d8f5ef41b68e3
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-02 01:16:19 +0100

    useEffect methods

--- RC-09-Lifecycle-useEffect/src/components/UseEffectHook.jsx
+++ RC-09-Lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -2,47 +2,45 @@
 //..                             USEEFFECT HOOK                               */
 /* -------------------------------------------------------------------------- */
 
-//! UseEffect Hook'u fonksiyonel componentler'te yan etkileri
-//! (side effect) gerceklestirmek icin kullanilir.
-//! componentDidMount,componentDidUpdate,ve componentWillUnmount
-//! metotlarinin bir birlesimi gibi dusunulebilir.
+//__    UseEffect Hook'u fonksiyonel componentler'te yan etkileri (side effect) gerçekleştirmek için kullanılır.
+//__    componentDidMount,componentDidUpdate,ve componentWillUnmount metotlarının bir birleşimi gibi düşünülebilir.
 
-//! useEffect(() => {
+//? useEffect(() => {
 //*   /* ComponentDidMount code */
 //*      Kodlar
-//! }, []);
+//? }, []);
 
-//! useEffect(() => {
+//? useEffect(() => {
 //*   */ ComponentDidMount + componentDidUpdate code */
-//! }, [state1, state2]);
+//? }, [state1, state2]);
 
-//! useEffect(() => {
-//?   /* ComponentDidMount code */
-//!   return () => {
+//? useEffect(() => {
+//*   /* ComponentDidMount code */
+//?   return () => {
 //*     //* componentWillUnmount code */
-//!   };
-//! }, []);
+//?   };
+//? }, []);
 
 import { useState, useEffect } from "react";
 
 const UseEffectHook = () => {
   const [count, setCount] = useState(0);
 
-  //! componentDidMount + componentWillUnmount
+  //.. componentDidMount + componentWillUnmount
   // useEffect(() => {
   //   console.log("2-componentDidMount")
 
-  //   //* clean-up funct. component DOM tree 'Dan kaldırıldıtkan sorna calisir.
+  //   //__ clean-up funct. component DOM tree 'Dan kaldırıldıtkan sorna calisir.
   //   return () => {
   //     console.log("4-componentWillUnmount")
   //   }
-  // }, []) //* dependency array boş olduğu için fonksiyonun gövdesi sadece ilk render sonrasında (bir kere) çalışır.
+  // }, []) //__ dependency array boş olduğu için fonksiyonun gövdesi sadece ilk render sonrasında (bir kere) çalışır.
 
   //! componentDidMount + componentDidUpdate
   useEffect(() => {
     console.log("2-componentDidMount");
     console.log("2-componentDidUpdate");
-  }, [count]); //? count state'i her degistiginde fonksiyonun gövdesi tekrar calisti. Bu durumda gövde hem mounting de hem de updating de calismis oldu.
+  }, [count]); //? count state'i her değiştiğinde fonksiyonun gövdesi tekrar çalıştı. Bu durumda gövde hem mounting de hem de updating de çalışmış oldu.
 
   //! componentWillUnmount
   useEffect(() => {
