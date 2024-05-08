90c42e2d68dcd4a2de97fb71e76d2558847a3c09
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-08 11:26:42 +0100

    spacing

--- RC-13-Tailwind/src/App.js
+++ RC-13-Tailwind/src/App.js
@@ -1,10 +1,12 @@
+import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typografy from "./components/Typografy";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
-      <Typografy />
+      {/* <Typografy /> */}
+      <SpacingAndSizing />
     </div>
   );
 }
---
+++ RC-13-Tailwind/src/components/SpacingAndSizing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SpacingAndSizing = () => {
+  return (
+    <>
+      <div>
+        <h3 className="text-2xl text-center">Spacing And Sizing</h3>
+        <button className="bg-blue-500 text-white p-4 m-1">Click</button>
+        <button className="bg-blue-500 text-white px-4">Click</button>
+        <button className="bg-blue-500 text-white px-4 ms-4">Click</button>
+        <button className="bg-blue-500 text-white px-4 ml-4">Click</button>
+        <button className="bg-blue-500 text-white px-4 py-2 mx-4">Click</button>
+      </div>
+
+      <div>
+        <button className="bg-green-500 text-white mx-4 px-4 py-2 border border-red-500 rounded-md ">
+          Click
+        </button>
+        <button className="bg-green-500 text-white mx-4 px-4 py-2 border-[3px] border-red-500 rounded-[10px] ">
+          Click
+        </button>
+        <button className="bg-green-500 text-white mx-4 px-6 py-6 w-20 h-20 rounded-full ">
+          Click
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SpacingAndSizing;
