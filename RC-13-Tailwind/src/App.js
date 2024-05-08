5b2a1b023b5fd30bf9399a0dd134a2281be3bf2f
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-08 11:49:50 +0100

    flex

--- RC-13-Tailwind/src/App.js
+++ RC-13-Tailwind/src/App.js
@@ -1,6 +1,7 @@
-import HoverAndFocus from "./components/HoverAndFocus";
-import SpacingAndSizing from "./components/SpacingAndSizing";
-import Typografy from "./components/Typografy";
+import FlexAndResponsive from "./components/FlexAndResponsive";
+// import HoverAndFocus from "./components/HoverAndFocus";
+// import SpacingAndSizing from "./components/SpacingAndSizing";
+// import Typografy from "./components/Typografy";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
       {/* <Typografy /> */}
       {/* <SpacingAndSizing /> */}
-      <HoverAndFocus />
+      {/* <HoverAndFocus /> */}
+      <FlexAndResponsive />
     </div>
   );
 }
---
+++ RC-13-Tailwind/src/components/FlexAndResponsive.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FlexAndResponsive = () => {
+  return (
+    <div className=" flex flex-row flex-wrap">
+      <div className=" bg-fuchsia-200 basis-1/4">01</div>
+      <div className=" bg-fuchsia-300 basis-1/4">02</div>
+      <div className=" bg-fuchsia-400 basis-1/4">03</div>
+      <div className=" bg-fuchsia-500 basis-1/4">04</div>
+      <div className=" bg-fuchsia-600 basis-2/4">05</div>
+      <div className=" bg-fuchsia-700 basis-2/4">06</div>
+    </div>
+  );
+};
+
+export default FlexAndResponsive;
