608bfc3ae1c99bcf174669fda49e46b1740abc53
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-08 10:54:12 +0100

    line clamp

--- RC-13-Tailwind/src/components/Typografy.jsx
+++ RC-13-Tailwind/src/components/Typografy.jsx
@@ -12,7 +12,27 @@ const Typografy = () => {
       <h3 className="text-[60px]">Typografy</h3>
       <h3 className="text-base/10">Typografy</h3>
 
-      
+      {/* //..Line Clamp */}
+      <article>
+        <p className="line-clamp-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+          quae dicta, libero, dolores aspernatur omnis ut placeat nemo quis
+          totam atque adipisci nisi harum cum, pariatur iste. Assumenda,
+          excepturi sunt expedita fuga sint ipsam officia, earum dolorum nam at
+          odit numquam fugiat qui voluptate facilis labore praesentium iure quo
+          enim quasi? Asperiores non ratione accusantium. Illum atque possimus
+          est dolorem necessitatibus fugiat, fuga quaerat praesentium ratione
+          labore facilis distinctio eos harum culpa nisi quia repellat veniam
+          aliquam soluta delectus. Quaerat perspiciatis, modi adipisci optio
+          dolores ab explicabo deleniti similique at harum quibusdam. Mollitia
+          architecto facilis, tempora officia tempore distinctio inventore.
+        </p>
+        <p className="text-center">Hello</p>
+        <p className="underline">Hello</p>
+        <p className="line-through">Hello</p>
+        <p className="uppercase">Hello</p>
+        <p className="lowercase">Hello</p>
+      </article>
     </div>
   );
 };
