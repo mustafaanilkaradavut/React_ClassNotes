5cacfe6dfe74b28d4d83849f6092107ff8b753b6
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-16 23:25:10 +0100

    toastify

--- RC-17_18-Movie-App/movie-app-s01-starter/src/App.js
+++ RC-17_18-Movie-App/movie-app-s01-starter/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div className="dark:bg-gray-dark-main main-h-screen">
       <AuthProvider>
         <AppRouter />
+        <ToastContainer />
       </AuthProvider>
     </div>
   );
--- RC-17_18-Movie-App/movie-app-s01-starter/src/context/AuthProvider.jsx
+++ RC-17_18-Movie-App/movie-app-s01-starter/src/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { createContext, useContext, useState } from "react";
 import { auth } from "../auth/firebase";
 import { useNavigate } from "react-router-dom";
@@ -29,7 +30,25 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-  const values = { currentUser, createUser };
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const values = { currentUser, createUser, signIn };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
