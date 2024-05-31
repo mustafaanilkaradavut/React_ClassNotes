6dcfee39d719303d858e4bca9f8c3a683fc16dd4
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-31 17:49:58 +0100

    devTools

--- RC-21-Redux-Example/redux-toolkit-starter/src/app/store.jsx
+++ RC-21-Redux-Example/redux-toolkit-starter/src/app/store.jsx
@@ -7,4 +7,6 @@ export const store = configureStore({
     login: loginReducer,
     news: newsReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
+  //? Eğer geliştirme aşaması production ise o zaman yukarıdaki ifade false dondurur ve dolayısıyla devTool kullanıma kapalı olur.
 });
