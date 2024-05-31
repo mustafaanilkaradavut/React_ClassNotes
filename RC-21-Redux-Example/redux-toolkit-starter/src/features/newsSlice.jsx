df25de164d3fb5d5b1f70c0cbbb977bec71f2949
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-05-31 16:35:04 +0100

    getNews

--- RC-21-Redux-Example/redux-toolkit-starter/src/features/newsSlice.jsx
+++ RC-21-Redux-Example/redux-toolkit-starter/src/features/newsSlice.jsx
@@ -1,6 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {};
+const initialState = {
+  newsData: [],
+  loading: false,
+  error: false,
+};
+
+const getNews = createAsyncThunk(
+  "getNews", //__ action type
+  async () => {
+    const API_KEY = "f33b836eabbd49888b8ef386ed77c55f";
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+    //__ api istek fonksiyonu
+    const data = await axios(url);
+  }
+);
 
 const newsSlice = createSlice({
   name: "news",
