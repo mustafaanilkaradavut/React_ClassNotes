3fba0efe4b12d76431239858af9d807ce18ff57c
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-05 02:42:22 +0100

    toastNotify

--- RC-22_26-Stock-App/stock-app-starter1/src/services/apiRequest.js
+++ RC-22_26-Stock-App/stock-app-starter1/src/services/apiRequest.js
@@ -1,12 +1,20 @@
 import axios from "axios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 export const login = async (userData) => {
-  const BASE_URL = "https://12138.fullstack.clarusway.com";
+  //   const BASE_URL = "https://12138.fullstack.clarusway.com";
+  //*  bu şekilde okuma yerine .env'de global bir state atarız.
 
   try {
-    const data = await axios.post(`${BASE_URL}/auth/login`, userData);
+    const data = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/auth/login`,
+      userData
+    );
+    toastSuccessNotify("Login is success");
     console.log(data);
+    return data;
   } catch (error) {
+    toastErrorNotify("Login is nonstarter");
     console.log(error);
   }
 };
