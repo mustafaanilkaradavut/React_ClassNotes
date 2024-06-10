012370e8cf20b81cc5fb51616c9b2659a3329928
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-10 03:46:15 +0100

    axios instance

--- RC-22_26-Stock-App/stock-app-starter1/src/services/useAxios.jsx
+++ RC-22_26-Stock-App/stock-app-starter1/src/services/useAxios.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { useSelector } from "react-redux";
 
+//__ axios instance kullanırız. const instance = axios.create ({baseURL,timeOut,headers})
+//__ axios kullanımımızı basit hale getirir.
+
+//? Custom Hook - token'ı state'in içerisinden çıkarmamız lazım. Çünkü token global state'ten okunabilir olmalı.
 const useAxios = () => {
   const { token } = useSelector((state) => state.auth);
 
+  //? Axios Instance
   const axiosToken = axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}`,
     headers: { Authorization: `Token ${token}` },
