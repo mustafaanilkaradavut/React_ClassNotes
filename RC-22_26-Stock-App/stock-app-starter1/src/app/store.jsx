0b5493f8fd09f9aeab134ca5157fdb5d5c5f586e
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-14 00:48:23 +0100

    fix: seriazable bug

--- RC-22_26-Stock-App/stock-app-starter1/src/app/store.jsx
+++ RC-22_26-Stock-App/stock-app-starter1/src/app/store.jsx
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PURGE,
+  REGISTER,
+  PERSIST,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 // import stroge from "redux-persist/lib/strorage/session" //__ session stroga
 
@@ -19,6 +28,12 @@ const store = configureStore({
     auth: persistedReducer,
     stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, REGISTER, PURGE, PERSIST],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
