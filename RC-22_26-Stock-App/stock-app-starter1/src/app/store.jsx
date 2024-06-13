f07ef7da9230d3dcaa57b0ed49e4ed17b9e893e5
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-14 00:36:36 +0100

    persistor

--- RC-22_26-Stock-App/stock-app-starter1/src/app/store.jsx
+++ RC-22_26-Stock-App/stock-app-starter1/src/app/store.jsx
@@ -1,12 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+// import stroge from "redux-persist/lib/strorage/session" //__ session stroga
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+//__ Burada hangi reducer'ı kalıcı belleğe atmak istiyorsak onu yazarız.
+//__ Birden fazla reducer var ise bunları combine etmek ve tek parça haline getirmek gerek.
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     stock: stockReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
+
 export default store;
