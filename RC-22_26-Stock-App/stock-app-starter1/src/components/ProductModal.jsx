fb83a8e269175b1fd284c447a3242ae7e1fd20b0
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-16 09:26:14 +0100

    Mui data grid

---
+++ RC-22_26-Stock-App/stock-app-starter1/src/components/ProductModal.jsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
+import useStockRequest from "../services/useStockRequest";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ProductModal({ handleClose, open, info, setInfo }) {
+  // const [info, setInfo] = React.useState({
+  //   name: "",
+  //   phone: "",
+  //   image: "",
+  //   address: "",
+  // });
+  // const [open, setOpen] = React.useState(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
+  //__ Yukarıdaki set işlemlerini Firms.jsx'e taşırız.
+
+  //__ Open State değişince local state resetlenir. Formu sumbit etmeden çıkınca tekrar girdiğimizde bilgiler resetlenir.
+  //__ Bu yapı'yı pasife alıyoruz çünkü bu yapı bizi edit kısmında bilgileri çekerken engelliyor.
+  // React.useEffect(() => {
+  //   setInfo({
+  //     name: "",
+  //     phone: "",
+  //     image: "",
+  //     address: "",
+  //   });
+  // }, [open]);
+
+  const { postStock, putStock } = useStockRequest();
+
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+    //__ Objeyi açarız ve value değiştiririz.
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (info._id) {
+      //? put isteğinin çağrılması
+      putStock("firms", info);
+    } else {
+      //? post firm işlemi
+      postStock("firms", info);
+    }
+
+    //? local state sıfırlama
+    // setInfo({ name: "", phone: "", address: "", image: "" });
+
+    //? modal kapama
+    handleClose();
+
+    //__   Post request (post firm) => Verileri sil => Modal'ı kapat
+  };
+
+  // console.log(info);
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component={"form"}
+            onSubmit={handleSubmit}>
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              value={info.name}
+              onChange={handleChange}
+              required
+            />
+            <TextField
+              label="Phone"
+              name="phone"
+              id="phone"
+              type="gsm"
+              variant="outlined"
+              value={info.phone}
+              onChange={handleChange}
+              required
+            />
+            <TextField
+              label="Address"
+              name="address"
+              id="address"
+              type="text"
+              variant="outlined"
+              value={info.address}
+              onChange={handleChange}
+              required
+            />
+            <Button variant="contained" type="submit">
+              {info._id ? "UPDATE FIRM" : "ADD FIRM"}
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- RC-22_26-Stock-App/stock-app-starter1/src/pages/Products.jsx
+++ RC-22_26-Stock-App/stock-app-starter1/src/pages/Products.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import * as React from "react";
+import useStockRequest from "../services/useStockRequest";
+import { useSelector } from "react-redux";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import FirmCard from "../components/FirmCard";
+import ProductTable from "../components/ProductTable";
+import ProductModal from "../components/ProductModal";
 
-const Products = () => {
-  return <div>Products</div>;
+const Firms = () => {
+  const { getStock } = useStockRequest();
+
+  //__ Bu kodu direk burada yazmamazın sebebi, başka yerlerde'de kullanmak için global'de yazıp dışarıya açarız.
+  const { firms } = useSelector((state) => state.stock);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    image: "",
+    address: "",
+  });
+  const handleClose = () => {
+    setOpen(false);
+    setInfo({
+      name: "",
+      phone: "",
+      image: "",
+      address: "",
+    });
+  };
+
+  useEffect(() => {
+    getStock("products");
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color={"error"} mb={2}>
+        Product
+      </Typography>
+      <Button variant="contained" onClick={handleOpen} sx={{ mb: 3 }}>
+        {/* Burada ki handleOpen işlemini "Lifting State Up" ile işleriz. */}
+        New Product
+      </Button>
+      <ProductModal
+        handleClose={handleClose}
+        open={open}
+        info={info}
+        setInfo={setInfo}
+      />
+      <ProductTable />
+    </div>
+  );
 };
 
-export default Products;
+export default Firms;
