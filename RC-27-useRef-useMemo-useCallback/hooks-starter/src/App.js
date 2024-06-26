71eab1e63fe46a615990e695e179c9b0970af333
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-06-26 03:00:56 +0100

    useRef

--- RC-27-useRef-useMemo-useCallback/hooks-starter/src/App.js
+++ RC-27-useRef-useMemo-useCallback/hooks-starter/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
 import TaxComp from "./components/TaxComp";
@@ -6,6 +6,21 @@ import TaxComp from "./components/TaxComp";
 function App() {
   const [count, setCount] = useState(0);
   const [user, setUser] = useState("MAK");
+  const [taxDataState, setTaxDataState] = useState({ taxRate: 0.18, ship: 25 });
+  //__ ilk oluşumda referansını tanımlıyor ve o referans sabit kalıyor.
+
+  // const taxData = {
+  //   taxRate: 0.18,
+  //   ship: 25,
+  // };
+  //__ Her render bellekteki referansı değiştiriyor.
+
+  //! useRef kullanılırken  useState hook'u kullanılmaz.
+  const taxDataRef = useRef({
+    taxRate: 0.18,
+    ship: 25,
+  });
+  //__ Hafızadaki referansı sabit ama kendisi değişebilir olan verileri tanımlamak istiyor.
 
   return (
     <div className="container mt-2">
@@ -36,7 +51,10 @@ function App() {
       {/* <HeaderMemo user={count} /> */}
       {/* Sürekli değişen değerlerimiz olacaksa memo mantıklı değildir.  */}
       <hr />
-      <TaxComp />
+      {/* <TaxComp taxData={taxData} />  Non Primitive type olarak gönderimde her render edilişte referans değiştiği için memo değişiklik varmış gibi algılayıp TaxComp yeniden render eder.*/}
+
+      {/* <TaxComp taxData={taxDataState} /> */}
+      <TaxComp taxData={taxDataRef} />
     </div>
   );
 }
--- RC-27-useRef-useMemo-useCallback/hooks-starter/src/components/TaxComp.jsx
+++ RC-27-useRef-useMemo-useCallback/hooks-starter/src/components/TaxComp.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { memo } from "react";
 
-const TaxComp = () => {
+const TaxComp = ({ taxData }) => {
+  console.log("Render => TaxData Component");
+  console.log(taxData);
   return <div>TaxComp</div>;
 };
 
-export default TaxComp;
+export default memo(TaxComp);
+
+//__ memo'nun diğer kullanımı component'i export kısmında sarmalamaktır.
