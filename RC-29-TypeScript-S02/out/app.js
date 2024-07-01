8e250d9be5aa12ff5da762e3171606815e093eb9
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-01 06:20:49 +0100

    interface shape

--- RC-29-TypeScript-S02/out/app.js
+++ RC-29-TypeScript-S02/out/app.js
@@ -11,3 +11,27 @@ const point1 = {
     x: 30,
     y: 50,
 };
+function calculateArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "rectangle":
+            return shape.width * shape.height;
+        default:
+            console.log("Someting went wrong");
+    }
+}
+const myCircle = {
+    kind: "circle",
+    radius: 10,
+    width: undefined,
+    height: undefined,
+};
+const myRect = {
+    kind: "rectangle",
+    height: 4,
+    width: 5,
+    radius: 0,
+};
+console.log(calculateArea(myCircle));
+console.log(calculateArea(myRect));
--- RC-29-TypeScript-S02/src/app.ts
+++ RC-29-TypeScript-S02/src/app.ts
@@ -26,3 +26,50 @@ const point1: Point = {
   x: 30,
   y: 50,
 };
+
+/* -------------------------------------------------------------------------- */
+interface Shape {
+  width: any;
+  radius: number;
+  height: any;
+  kind: "circle" | "rectangle";
+}
+
+interface Circle extends Shape {
+  kind: "circle";
+  radius: number;
+}
+
+interface Rectangle extends Shape {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+
+function calculateArea(shape: Circle): number;
+function calculateArea(shape: Rectangle): number;
+function calculateArea(shape: Shape): number | undefined {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "rectangle":
+      return shape.width * shape.height;
+    default:
+      console.log("Someting went wrong");
+  }
+}
+
+const myCircle: Circle = {
+  kind: "circle",
+  radius: 10,
+  width: undefined,
+  height: undefined,
+};
+const myRect: Rectangle = {
+  kind: "rectangle",
+  height: 4,
+  width: 5,
+  radius: 0,
+};
+console.log(calculateArea(myCircle));
+console.log(calculateArea(myRect));
