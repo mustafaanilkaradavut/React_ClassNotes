266751b8a1474a5a2c1bcd4f1cfe4c461c38d972
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2024-07-01 02:06:15 +0100

    enum

--- RC-28-TypeScript-S01/out/app.js
+++ RC-28-TypeScript-S01/out/app.js
@@ -4,4 +4,16 @@ myTuple3 = [
     [1, "jhon"],
     [2, "smith"],
 ];
-myTuple3.push([3, "Jane"]);
+let selectedColor = 2;
+console.log(selectedColor);
+let userSize = "S";
+console.log(userSize);
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+    StatusCodes[StatusCodes["Success"] = 200] = "Success";
+    StatusCodes[StatusCodes["Accepted"] = 202] = "Accepted";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+    StatusCodes[StatusCodes["ServerError"] = 401] = "ServerError";
+})(StatusCodes || (StatusCodes = {}));
+console.log(StatusCodes.ServerError);
--- RC-28-TypeScript-S01/src/app.ts
+++ RC-28-TypeScript-S01/src/app.ts
@@ -56,4 +56,46 @@ myTuple3 = [
 // myTuple3.push([3, "Jane"]); //* okey
 // myTuple3.push(false); //! error because boolean değer.
 
+//.. Enum
 
+const enum Color {
+  Red,
+  Green,
+  Blue,
+}
+
+//__ const kullanmazsak JS aşağıdaki gibi olur.
+/*
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let selectedColor = Color.Blue;
+console.log(selectedColor);
+*/
+
+let selectedColor: Color = Color.Blue;
+console.log(selectedColor);
+
+const enum Tshirt {
+  Small = "S",
+  Medium = "M",
+  Large = "L",
+}
+
+let userSize: Tshirt = Tshirt.Small;
+console.log(userSize);
+
+enum StatusCodes {
+  NotFound = 404,
+  Success = 200,
+  Accepted = Success + 2,
+  BadRequest = NotFound - 4,
+  ServerError, //__ Eğer burada değer vermezsek. Vermediğimiz andan itibaren önceki değer +1 şeklinde devam eder.
+  userDetail = "ANIL",
+  // userError, //__ Bu durumda eğer değerimiz string ise otomatik atama yapamaz hata verir.
+}
+
+console.log(StatusCodes.ServerError);
